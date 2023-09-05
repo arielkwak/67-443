c5fabccf1cb1201cbe6575cd2ae4956a08018aa8
Ariel Kwak <anniekwak123@gmail.com>
2023-09-04 22:31:36 -0400

    Save progress where time interval is formatted

---
+++ Stopwatch/Stopwatch.swift
@@ -0,0 +1,51 @@
+//
+//  Stopwatch.swift
+//  Stopwatch
+//
+//  Created by Ariel Kwak on 9/4/23.
+//
+
+import Foundation
+class Stopwatch {
+  
+  private var startTime: NSDate?
+
+  func start() {
+    startTime = NSDate()
+  }
+
+  func stop() {
+    startTime = nil
+  }
+   
+  var elapsedTime: TimeInterval {
+    if let startTime = self.startTime {
+      return -1 * startTime.timeIntervalSinceNow // could also just say -startTime.timeIntervalSinceNow
+    } else {
+      return 0
+    }
+  }
+  
+  var elapsedTimeAsString: String {
+      if let startTime = startTime {
+          // Calculate elapsed time in seconds
+          let elapsedTime = startTime.timeIntervalSinceNow
+
+          // Calculate minutes, seconds, and fractions of a second
+          let minutes = Int(elapsedTime / 60)
+          let seconds = Int(elapsedTime.truncatingRemainder(dividingBy: 60))
+          let tenthsOfASecond = Int((elapsedTime.truncatingRemainder(dividingBy: 1)) * 10)
+
+          // Create a formatted string
+          return String(format: "%02d:%02d.%d", minutes, seconds, tenthsOfASecond)
+      } else {
+          // Handle the case when startTime is nil
+          return "00:00.0"
+      }
+  }
+  
+  var isRunning: Bool {
+      return startTime != nil
+  }
+
+}
--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var elapsedTimeLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+  }
+
+  @IBAction func startButtonTapped(sender: UIButton) {
+    // code to start the clock
+  }
+
+  @IBAction func stopButtonTapped(sender: UIButton) {
+    // code to stop the clock
   }
 
 
